373ddfe64e3920ba8bf771de88de90887a65ffa0
Leon Tofel <leo.tofel@gmail.com>
2022-11-07 10:45:45 -0300

    fix: add minor fixes

--- apps/client/src/main.tsx
+++ apps/client/src/main.tsx
@@ -1,37 +1,10 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App'
-import { ChakraProvider, ColorModeScript, extendTheme } from "@chakra-ui/react";
-import '@fontsource/montserrat/700.css';
-import '@fontsource/montserrat/400.css';
-import '@fontsource/montserrat/300.css';
-import { createClient, defaultExchanges, Provider, subscriptionExchange } from 'urql';
-import { createClient as createWSClient } from 'graphql-ws';
-
-const theme = extendTheme({
-    fonts: {
-        heading: 'Montserrat',
-        body: 'Montserrat'
-    }
-});
-
-const wsClient = createWSClient({
-    url: 'http://localhost:5000/graphql'
-})
-
-const client = createClient({
-    url: 'http://localhost:5000/graphql',
-    exchanges: [
-        ...defaultExchanges,
-        subscriptionExchange({
-            forwardSubscription: (operation) => ({
-                subscribe: (sink: any) => ({
-                    unsubscribe: wsClient.subscribe(operation, sink),
-                })
-            })
-        })
-    ]
-});
+import { ChakraProvider, ColorModeScript } from "@chakra-ui/react";
+import { Provider } from 'urql';
+import { theme } from "./utils/chakra";
+import { client } from "./utils/graphql";
 
 ReactDOM.createRoot(document.getElementById('root') as HTMLElement).render(
   <React.StrictMode>
---
+++ apps/client/src/utils/graphql.ts
@@ -0,0 +1,20 @@
+import { createClient, defaultExchanges, subscriptionExchange } from 'urql';
+import { createClient as createWSClient } from 'graphql-ws';
+
+export const wsClient = createWSClient({
+    url: 'http://localhost:5000/graphql'
+});
+
+export const client = createClient({
+    url: 'http://localhost:5000/graphql',
+    exchanges: [
+        ...defaultExchanges,
+        subscriptionExchange({
+            forwardSubscription: (operation) => ({
+                subscribe: (sink: any) => ({
+                    unsubscribe: wsClient.subscribe(operation, sink),
+                })
+            })
+        })
+    ]
+});
